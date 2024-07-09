7ab6e766d0b359369dcdb0e83a4268251c219ded
Hemeswari Sekar <126307610+Hemeswari-S@users.noreply.github.com>
2024-07-09 12:33:12 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -9,21 +9,19 @@ import fs from "fs";
 
 const app = express();
 
-const corsOptions = {
-  origin: "*",
-  methods: "POST, GET, PUT,DELETE",
-  "preflightContinue": false,
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-app.use(cors(corsOptions));
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "*");
-//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-//   next();
-// });
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 connectToDB();
 
 const options = {
