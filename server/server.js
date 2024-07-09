89a010fda43b8fd79d14932056369871a2b9f7fa
Hemeswari-S <hemeswarisekar3@gmail.com>
2024-07-09 11:52:48 +0530

    Server.js

--- server/server.js
+++ server/server.js
@@ -5,58 +5,34 @@ import { connectToDB } from "./config/connectTODB.js";
 import { User_route } from "./Routes/User_Root.js";
 import { Sign_route } from "./Routes/SignUp&In_Root.js";
 import https from 'https';
-import fs from 'fs';
+import fs from 'fs'
 
 const app = express();
 
-const corsOptions = {
-  origin: 'https://crudapp-client.onrender.com',
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-};
-
-app.use((req, res, next) => {
-  console.log('Incoming Request:', {
-    method: req.method,
-    url: req.url,
-    headers: req.headers
-  });
-  next();
-});
-
-app.use(cors(corsOptions));
-app.options('*', cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
+connectToDB();
 app.use((req, res, next) => {
-  res.on('finish', () => {
-    console.log('Outgoing Response:', {
-      statusCode: res.statusCode,
-      headers: res.getHeaders()
-    });
-  });
+  res.header("Access-Control-Allow-Origin", "*"); // Allow requests from any origin
+  res.header("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE"); // Allow these HTTP methods
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization"); // Allow these headers
   next();
 });
 
-connectToDB();
-
 const options = {
   key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert'),
+  cert: fs.readFileSync('server.cert')
 };
 
-app.get('/test-cors', (req, res) => {
-  res.json({ message: 'CORS is working!' });
-});
 
 app.use("/", User_route);
-app.use("/signup", cors(corsOptions), Sign_route); 
+app.use("/signup", Sign_route);
 app.all("*", (req, res) => {
   res.send("invalid URL..!");
 });
-
 https.createServer(options, app).listen(process.env.PORT, () => {
   console.log(`Backend server running on https://localhost:${process.env.PORT}`);
 });
+
