b3352804fcd086d32367be843f5a33df6dbb1a0c
Hemeswari-S <hemeswarisekar3@gmail.com>
2024-07-09 12:15:38 +0530

    Hemeswari

--- server/server.js
+++ server/server.js
@@ -10,17 +10,19 @@ import fs from "fs";
 const app = express();
 
 const corsOptions = {
-  origin: "https://crudapp-client.onrender.com/signup",
+  origin: "*",
+  methods: "POST, GET, PUT,DELETE",
+  allowedHeaders: ["Content-Type", "Authorization"],
 };
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  next();
-});
+// app.use((req, res, next) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+//   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+//   next();
+// });
 connectToDB();
 
 const options = {
