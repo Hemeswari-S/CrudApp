fce3ff63db47eb1943a7fe957ff14376bee7703b
Hemeswari-S <hemeswarisekar3@gmail.com>
2024-07-09 12:09:51 +0530

    Origin

--- server/server.js
+++ server/server.js
@@ -4,26 +4,29 @@ import bodyParser from "body-parser";
 import { connectToDB } from "./config/connectTODB.js";
 import { User_route } from "./Routes/User_Root.js";
 import { Sign_route } from "./Routes/SignUp&In_Root.js";
-import https from 'https';
-import fs from 'fs'
+import https from "https";
+import fs from "fs";
 
 const app = express();
 
 const corsOptions = {
-  origin: "https://crudapp-client.onrender.com/signup" 
-}
+  origin: "https://crudapp-client.onrender.com/signup",
+};
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
 connectToDB();
 
-
 const options = {
-  key: fs.readFileSync('server.key'),
-  cert: fs.readFileSync('server.cert')
+  key: fs.readFileSync("server.key"),
+  cert: fs.readFileSync("server.cert"),
 };
-
 
 app.use("/", User_route);
 app.use("/signup", Sign_route);
@@ -31,6 +34,7 @@ app.all("*", (req, res) => {
   res.send("invalid URL..!");
 });
 https.createServer(options, app).listen(process.env.PORT, () => {
-  console.log(`Backend server running on https://localhost:${process.env.PORT}`);
+  console.log(
+    `Backend server running on https://localhost:${process.env.PORT}`
+  );
 });
-
